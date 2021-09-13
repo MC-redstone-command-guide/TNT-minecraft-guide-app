a0520aa27cd611f795ea2170f2828e32af842cf0
ShinMyeongJi <shinmj1207@gmail.com>
2021-09-13 21:38:24 +0900

    splash screen(로딩화면) 추가

--- TNTApp/.eslintrc.js
+++ TNTApp/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   root: true,
+  useTabs: false,
   extends: '@react-native-community',
 };
---
+++ TNTApp/android/app/src/main/java/com/test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, R.style.SplashScreenTheme);
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  protected String getMainComponentName() {
+    return "TNT";
+  }
+}
